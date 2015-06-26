521e34f745ecc7139755d4edad9c778179b291d6
DigohD <digohd1337@gmail.com>
2015-06-26 16:23:05 +0200

    Test1

---
+++ UNet Test Project/Assets/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Left")){
+			GetComponent<Rigidbody>().AddForce(new Vector3(100, 0, 0));
+		}
+
+		if (Input.GetButtonDown("Right")){
+			GetComponent<Rigidbody>().AddForce(new Vector3(-100, 0, 0));
+		}
+
+		if (Input.GetButtonDown("Jump")){
+			GetComponent<Rigidbody>().AddForce(new Vector3(0, 400, 0));
+		}
+	}
+}
