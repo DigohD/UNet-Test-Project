d050ff7225dfbb7f247f3c1074318ad908f8ae63
DigohD <digohd1337@gmail.com>
2015-06-26 16:40:11 +0200

    LERP

--- UNet Test Project/Assets/PlayerControl.cs
+++ UNet Test Project/Assets/PlayerControl.cs
@@ -7,6 +7,8 @@ public class PlayerControl : NetworkBehaviour {
 	[SyncVar]
 	Vector3 syncPos;
 
+	float lerpRate = 15;
+
 	// Use this for initialization
 	void Start () {
 		syncPos = transform.position;
@@ -15,29 +17,33 @@ public class PlayerControl : NetworkBehaviour {
 	void FixedUpdate(){
 		updatePositions();
 		updateOtherClients();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
 		if(!isLocalPlayer)
 			return;
-
+		
 		if (Input.GetButtonDown("Left")){
 			GetComponent<Rigidbody>().AddForce(new Vector3(100, 0, 0));
 		}
-
+		
 		if (Input.GetButtonDown("Right")){
 			GetComponent<Rigidbody>().AddForce(new Vector3(-100, 0, 0));
 		}
-
+		
 		if (Input.GetButtonDown("Jump")){
 			GetComponent<Rigidbody>().AddForce(new Vector3(0, 400, 0));
 		}
 	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
 
 	void updateOtherClients(){
 		if(!isLocalPlayer)
-			transform.position = syncPos;
+			transform.position = Vector3.Lerp(transform.position, syncPos, Time.deltaTime * lerpRate);
+
+
 	}
 
 	[Command]
