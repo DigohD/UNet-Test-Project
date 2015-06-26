d4540c1d30d8f12aa9599992ea5022670ace8e71
DigohD <digohd1337@gmail.com>
2015-06-26 15:29:44 +0200

    Init

---
+++ UNet Test Project/Assets/Scripts/Player_Setup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_Setup : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.LogWarning("asfafsa");
+		if(isLocalPlayer){
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
