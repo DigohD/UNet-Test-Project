7ac6320693d6bc2be6efa6cbef0d0bb27796893d
DigohD <digohd1337@gmail.com>
2015-06-26 16:32:44 +0200

    Test3

--- UNet Test Project/Assets/PlayerControl.cs
+++ UNet Test Project/Assets/PlayerControl.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : NetworkBehaviour {
+
+	[SyncVar]
+	Vector3 syncPos;
 
 	// Use this for initialization
 	void Start () {
-	
+		syncPos = transform.position;
+	}
+
+	void FixedUpdate(){
+		updatePositions();
+		updateOtherClients();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(!isLocalPlayer)
+			return;
+
 		if (Input.GetButtonDown("Left")){
 			GetComponent<Rigidbody>().AddForce(new Vector3(100, 0, 0));
 		}
@@ -21,5 +33,21 @@ public class PlayerControl : MonoBehaviour {
 		if (Input.GetButtonDown("Jump")){
 			GetComponent<Rigidbody>().AddForce(new Vector3(0, 400, 0));
 		}
+	}
+
+	void updateOtherClients(){
+		if(!isLocalPlayer)
+			transform.position = syncPos;
+	}
+
+	[Command]
+	public void Cmd_Move(Vector3 newPos){
+		syncPos = newPos;
+		transform.position = syncPos;
+	}
+
+	[ClientCallback]
+	void updatePositions(){
+		Cmd_Move(transform.position);
 	}
 }
